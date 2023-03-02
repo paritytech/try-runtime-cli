a973aaa9adc051b0540f202837066700bf1c6ca1
Piotr Mikołajczyk <piomiko41@gmail.com>
2023-03-02 09:09:15 +0100

    Adapt to new subxt

--- cli/main.rs
+++ cli/main.rs
@@ -7,9 +7,22 @@ use try_runtime_core::commands::TryRuntime;
 
 #[subxt::subxt(
     runtime_metadata_url = "wss://rpc.polkadot.io:443",
-    derive_for_all_types = "Clone, PartialEq, Eq"
+    derive_for_all_types = "Clone, PartialEq, Eq",
+    runtime_types_only
 )]
 pub mod polkadot {}
+
+type RuntimeCall = polkadot::runtime_types::polkadot_runtime::RuntimeCall;
+
+type UncheckedExtrinsic = generic::UncheckedExtrinsic<
+    <PolkadotConfig as subxt::Config>::Address,
+    RuntimeCall,
+    <PolkadotConfig as subxt::Config>::Signature,
+    (),
+>;
+
+type Header = generic::Header<u32, BlakeTwo256>;
+type Block = generic::Block<Header, UncheckedExtrinsic>;
 
 fn init_env() {
     if env::var(env_logger::DEFAULT_FILTER_ENV).is_err() {
@@ -18,22 +31,12 @@ fn init_env() {
     env_logger::init();
 }
 
-type UncheckedExtrinsic = generic::UncheckedExtrinsic<
-    <PolkadotConfig as subxt::Config>::Address,
-    polkadot::runtime_types::polkadot_runtime::RuntimeCall,
-    <PolkadotConfig as subxt::Config>::Signature,
-    (),
->;
-type Header = generic::Header<u32, BlakeTwo256>;
-type Block = generic::Block<Header, UncheckedExtrinsic>;
-
 #[tokio::main]
 async fn main() {
     init_env();
 
     let cmd = TryRuntime::parse();
-    cmd.run::<Block,
-    sc_executor::sp_wasm_interface::ExtendedHostFunctions<sp_io::SubstrateHostFunctions, ()>>()
+    cmd.run::<Block, sc_executor::sp_wasm_interface::ExtendedHostFunctions<sp_io::SubstrateHostFunctions, ()>>()
         .await
         .unwrap();
 }
