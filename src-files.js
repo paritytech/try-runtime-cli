a088cec73b8462c7171251d0268386e2418635b3
liamaharon <liamaharon@users.noreply.github.com>
2023-10-10 15:18:14 +0000

    deploy: 793b3181f379d60cf09313cad2bc52dbdb6ec5fb

--- src-files.js
+++ src-files.js
@@ -1,5 +1,5 @@
-var sourcesIndex = JSON.parse('{\
+var srcIndex = JSON.parse('{\
 "try_runtime":["",[],["main.rs"]],\
 "try_runtime_core":["",[["commands",[],["create_snapshot.rs","execute_block.rs","fast_forward.rs","follow_chain.rs","mod.rs","offchain_worker.rs","on_runtime_upgrade.rs"]]],["inherent_provider.rs","lib.rs","parse.rs","shared_parameters.rs","state.rs"]]\
 }');
-createSourceSidebar();
+createSrcSidebar();
