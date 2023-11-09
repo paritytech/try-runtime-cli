09a9d6c0a11d399afc7b4f3295b83c4d891fe5a3
Liam Aharon <liam.aharon@hotmail.com>
2023-11-09 08:37:10 +0400

    split up test cases

--- core/tests/on_runtime_upgrade.rs
+++ core/tests/on_runtime_upgrade.rs
@@ -17,89 +17,110 @@
 
 #![cfg(unix)]
 
-use std::time::Duration;
+mod on_runtime_upgrade {
+    use std::time::Duration;
 
-use assert_cmd::cargo::cargo_bin;
-use substrate_cli_test_utils as common;
-use tokio::process::Command;
+    use assert_cmd::cargo::cargo_bin;
+    use substrate_cli_test_utils as common;
+    use tokio::process::Command;
 
-#[tokio::test]
-async fn on_runtime_upgrade_works() {
-    common::run_with_timeout(Duration::from_secs(60), async move {
+    fn on_runtime_upgrade(
+        snap_path: &str,
+        runtime_path: &str,
+        extra_args: &[&str],
+    ) -> tokio::process::Child {
+        Command::new(cargo_bin("try-runtime"))
+            .stdout(std::process::Stdio::piped())
+            .stderr(std::process::Stdio::piped())
+            .arg(format!("--runtime={}", runtime_path))
+            .arg("on-runtime-upgrade")
+            .args(extra_args)
+            .args(["snap", format!("--path={}", snap_path).as_str()])
+            .kill_on_drop(true)
+            .spawn()
+            .unwrap()
+    }
+
+    #[tokio::test]
+    async fn ok_works() {
+        common::run_with_timeout(Duration::from_secs(60), async move {
+            let project_root = env!("CARGO_MANIFEST_DIR");
+            let snap_path = format!("{}/tests/snaps/rococo-bridge-hub.snap", project_root);
+            let runtime_path = format!(
+                "{}/tests/runtimes/bridge_hub_rococo_runtime_OK.compact.compressed.wasm",
+                project_root
+            );
+            let child = on_runtime_upgrade(snap_path.as_str(), runtime_path.as_str(), &[]);
+            let out = child.wait_with_output().await.unwrap();
+            assert!(out.status.success());
+        })
+        .await;
+    }
+
+    #[tokio::test]
+    async fn weight_issue_fails() {
+        common::run_with_timeout(Duration::from_secs(60), async move {
+            let project_root = env!("CARGO_MANIFEST_DIR");
+            let snap_path = format!("{}/tests/snaps/rococo-bridge-hub.snap", project_root);
+            let runtime_path = format!(
+                "{}/tests/runtimes/bridge_hub_rococo_runtime_WEIGHT_ISSUE.compact.compressed.wasm",
+                project_root
+            );
+            let child = on_runtime_upgrade(snap_path.as_str(), runtime_path.as_str(), &[]);
+            let out = child.wait_with_output().await.unwrap();
+            assert!(!out.status.success());
+        })
+        .await;
+    }
+
+    #[tokio::test]
+    async fn weight_issue_can_be_ignored() {
+        common::run_with_timeout(Duration::from_secs(300), async move {
+            let project_root = env!("CARGO_MANIFEST_DIR");
+            let snap_path = format!("{}/tests/snaps/rococo-bridge-hub.snap", project_root);
+            let runtime_path = format!(
+                "{}/tests/runtimes/bridge_hub_rococo_runtime_WEIGHT_ISSUE.compact.compressed.wasm",
+                project_root
+            );
+
+            let child = on_runtime_upgrade(
+                snap_path.as_str(),
+                runtime_path.as_str(),
+                &["--no-weight-warnings"],
+            );
+            let out = child.wait_with_output().await.unwrap();
+            assert!(out.status.success());
+        })
+        .await;
+    }
+
+    #[tokio::test]
+    async fn not_idempotent_execution_fails() {
+        common::run_with_timeout(Duration::from_secs(300), async move {
         let project_root = env!("CARGO_MANIFEST_DIR");
-        fn on_runtime_upgrade(
-            snap_path: &str,
-            runtime_path: &str,
-            extra_args: &[&str],
-        ) -> tokio::process::Child {
-            Command::new(cargo_bin("try-runtime"))
-                .stdout(std::process::Stdio::piped())
-                .stderr(std::process::Stdio::piped())
-                .arg(format!("--runtime={}", runtime_path))
-                .arg("on-runtime-upgrade")
-                .args(extra_args)
-                .args(["snap", format!("--path={}", snap_path).as_str()])
-                .kill_on_drop(true)
-                .spawn()
-                .unwrap()
-        }
-
-        println!("Checking OK migration");
         let snap_path = format!("{}/tests/snaps/rococo-bridge-hub.snap", project_root);
-        let runtime_path = format!(
-            "{}/tests/runtimes/bridge_hub_rococo_runtime_OK.compact.compressed.wasm",
-            project_root
-        );
-        let child = on_runtime_upgrade(snap_path.as_str(), runtime_path.as_str(), &[]);
-        let out = child.wait_with_output().await.unwrap();
-        assert!(out.status.success());
-
-        println!("Checking migration with weight issue fails");
-        let runtime_path = format!(
-            "{}/tests/runtimes/bridge_hub_rococo_runtime_WEIGHT_ISSUE.compact.compressed.wasm",
-            project_root
-        );
-        let child = on_runtime_upgrade(snap_path.as_str(), runtime_path.as_str(), &[]);
-        let out = child.wait_with_output().await.unwrap();
-        assert!(!out.status.success());
-
-        println!("Checking weight issues can be ignored with an arg");
-        let child = on_runtime_upgrade(
-            snap_path.as_str(),
-            runtime_path.as_str(),
-            &["--no-weight-warnings"],
-        );
-        let out = child.wait_with_output().await.unwrap();
-        assert!(out.status.success());
-
-        println!("Checking migration with idempotency execution issue fails");
         let runtime_path = format!(
             "{}/tests/runtimes/bridge_hub_rococo_runtime_NOT_IDEMPOTENT_EXECUTION.compact.compressed.wasm",
             project_root
         );
+
         let child = on_runtime_upgrade(snap_path.as_str(), runtime_path.as_str(), &[]);
         let out = child.wait_with_output().await.unwrap();
         assert!(!out.status.success());
+    })
+    .await;
+    }
 
-        println!("Checking idempotency execution issue can be ignored with an arg");
-        let child = on_runtime_upgrade(
-            snap_path.as_str(),
-            runtime_path.as_str(),
-            &["--no-idempotency-checks"],
-        );
-        let out = child.wait_with_output().await.unwrap();
-        assert!(out.status.success());
-
-        println!("Checking migration with idempotency state root issue fails");
+    #[tokio::test]
+    async fn not_idempotent_execution_issue_can_be_ignored() {
+        common::run_with_timeout(Duration::from_secs(300), async move {
+        let project_root = env!("CARGO_MANIFEST_DIR");
+        let snap_path = format!("{}/tests/snaps/rococo-bridge-hub.snap", project_root);
         let runtime_path = format!(
-            "{}/tests/runtimes/bridge_hub_rococo_runtime_NOT_IDEMPOTENT_STATE_ROOT.compact.compressed.wasm",
+            "{}/tests/runtimes/bridge_hub_rococo_runtime_NOT_IDEMPOTENT_EXECUTION.compact.compressed.wasm",
             project_root
         );
-        let child = on_runtime_upgrade(snap_path.as_str(), runtime_path.as_str(), &[]);
-        let out = child.wait_with_output().await.unwrap();
-        assert!(!out.status.success());
 
-        println!("Checking idempotency state root issue can be ignored with an arg");
         let child = on_runtime_upgrade(
             snap_path.as_str(),
             runtime_path.as_str(),
@@ -109,4 +130,42 @@ async fn on_runtime_upgrade_works() {
         assert!(out.status.success());
     })
     .await;
+    }
+
+    #[tokio::test]
+    async fn not_idempotent_state_root_fails() {
+        common::run_with_timeout(Duration::from_secs(300), async move {
+        let project_root = env!("CARGO_MANIFEST_DIR");
+        let snap_path = format!("{}/tests/snaps/rococo-bridge-hub.snap", project_root);
+        let runtime_path = format!(
+            "{}/tests/runtimes/bridge_hub_rococo_runtime_NOT_IDEMPOTENT_EXECUTION.compact.compressed.wasm",
+            project_root
+        );
+
+        let child = on_runtime_upgrade(snap_path.as_str(), runtime_path.as_str(), &[]);
+        let out = child.wait_with_output().await.unwrap();
+        assert!(!out.status.success());
+    })
+    .await;
+    }
+
+    #[tokio::test]
+    async fn not_idempotent_state_root_issue_can_be_ignored() {
+        common::run_with_timeout(Duration::from_secs(300), async move {
+        let project_root = env!("CARGO_MANIFEST_DIR");
+        let snap_path = format!("{}/tests/snaps/rococo-bridge-hub.snap", project_root);
+        let runtime_path = format!(
+            "{}/tests/runtimes/bridge_hub_rococo_runtime_NOT_IDEMPOTENT_EXECUTION.compact.compressed.wasm",
+            project_root
+        );
+        let child = on_runtime_upgrade(
+            snap_path.as_str(),
+            runtime_path.as_str(),
+            &["--no-idempotency-checks"],
+        );
+        let out = child.wait_with_output().await.unwrap();
+        assert!(out.status.success());
+    })
+    .await;
+    }
 }
