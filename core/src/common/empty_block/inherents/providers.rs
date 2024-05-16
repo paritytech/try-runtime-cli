828a3579da53041399c39bd366e33e09fe00d96a
Liam Aharon <liam.aharon@hotmail.com>
2024-05-16 10:48:29 +0400

    doc

--- core/src/common/empty_block/inherents/providers.rs
+++ core/src/common/empty_block/inherents/providers.rs
@@ -14,7 +14,8 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-//! TODO: Docs
+
+//! Contains providers for inherents required for empty block production.
 
 use std::{
     str::FromStr,
